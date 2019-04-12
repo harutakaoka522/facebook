1b62236b4d9b51c76c71746fea903d02ff307109
高岡春樹 <takaokaharuki@takaokaharukinoMacBook-puro.local>
2019-04-12 13:48:24 +0900

    ブランクで投稿できる状態を修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+    validates :content, presence: true
+    validates :image, presence: true
     belongs_to :user, optional: true
     mount_uploader :image, ImageUploader
 end
